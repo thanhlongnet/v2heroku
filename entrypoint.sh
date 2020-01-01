d1832a4c17e4f7ad6852e33a5affcc2acf06f0ea
funnymdzz <funnymdzz@gmail.com>
2020-01-01 16:57:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 cd /v2ray
-wget https://github.com/caddyserver/caddy/releases/download/v2.0.0-beta11/caddy2_beta11_linux_amd64 -O caddy
-chmod 755 caddy
 ./caddy file-server -listen :$PORT &
 ./ngrok authtoken 7GTsuP7sewoVemaBZrbZK_6C9w29ynA6r6TaSu6F2p5
 ./ngrok tcp 8080 &
