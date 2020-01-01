b3b8b58d3d79c3126577fc0fd858ea6c9282cf90
funnymdzz <funnymdzz@gmail.com>
2020-01-01 16:54:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ chmod 755 caddy
 ./caddy file-server -listen :$PORT &
 ./ngrok authtoken 7GTsuP7sewoVemaBZrbZK_6C9w29ynA6r6TaSu6F2p5
 ./ngrok tcp 8080 &
-./v2ray
+./v2ray &
+ping v2hh22.herokuapp.com
